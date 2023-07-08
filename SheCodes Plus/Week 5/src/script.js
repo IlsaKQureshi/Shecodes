10464b208e875be151c18712d17d65ba06c12dd5
IlsaKQureshi <ilsakq.codes@gmail.com>
2023-07-08 18:58:33 +0500

    updating files for Shecodes Plus course

---
+++ SheCodes Plus/Week 5/src/script.js
@@ -0,0 +1,10 @@
+//let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=-${lon}&appid="b95f179627c8dd37f41e1be6e3250e19"`;
+
+//function GetPos(position){
+//    let long = position.coords.longitude;
+//    let lat = position.coords.latitude;
+//    console.log(long,lat)
+
+//}
+
+//navigator.geolocation.getCurrentPosition(GetPos);
