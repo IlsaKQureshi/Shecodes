a91963896f361627dee379a24d4f0413646e1d37
IlsaKQureshi <ilsakq.codes@gmail.com>
2023-09-12 04:29:17 +0500

    added icon from API

--- SheCodes Plus/Week 7/src/script.js
+++ SheCodes Plus/Week 7/src/script.js
@@ -33,6 +33,7 @@ function show_temp(response) {
   let humidity_element = document.querySelector("#humidity");
   let wind_element = document.querySelector("#wind");
   let date_element = document.querySelector("#date");
+  let weather_icon_element = document.querySelector("#weather_icon");
 
   temp_element.innerHTML = Math.round(response.data.main.temp);
   city_element.innerHTML = response.data.name;
@@ -40,10 +41,19 @@ function show_temp(response) {
   humidity_element.innerHTML = response.data.main.humidity;
   wind_element.innerHTML = Math.round(response.data.wind.speed);
   date_element.innerHTML = formatDate(response.data.dt * 1000);
+  weather_icon_element.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weather_icon_element.setAttribute(
+    "alt",
+    response.data.weather[0].description
+  );
 }
 
 let apiKey = `88d15ed9f618b4c3fad2d4f40f91aa94`;
-let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Karachi";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(url);
 axios.get(url).then(show_temp);
