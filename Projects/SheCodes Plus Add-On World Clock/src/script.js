b39d739aae0d9bcf92881c93428b6f1202b37114
IlsaKQureshi <ilsakq.codes@gmail.com>
2024-01-07 17:34:34 +0500

    Added select option for different time zones

--- Projects/SheCodes Plus Add-On World Clock/src/script.js
+++ Projects/SheCodes Plus Add-On World Clock/src/script.js
@@ -1,40 +1,3 @@
-function localTime() {
-  let locationTime = document.querySelector("#loc_time");
-  let locationCity = document.querySelector("#loc_city");
-  let locationDate = document.querySelector("#loc_date");
-  let locationTZ = moment.tz.guess();
-  let cityTime = moment().tz(locationTZ);
-
-  locationCity = locationTZ.replace("_", " ").split("/")[1];
-
-  let locationElement = document.getElementById("location");
-
-  locationElement.innerHTML = `<div class="location" id="location">
-        <div class="time" id="loc_time">${cityTime.format(
-          "h:mm:ss"
-        )}<small> ${cityTime.format("A")}</small></div>
-        <div class="city" id="loc_city">${locationCity}</div>
-        <div class="date" id="loc_date">${cityTime.format("MMMM Do YYYY")}</div>
-      </div>`;
-
-  let hr = document.getElementById("hour");
-  let mn = document.getElementById("min");
-  let sc = document.getElementById("sec");
-
-  let hh = cityTime.hour();
-  let mm = cityTime.minute();
-  let ss = cityTime.second();
-
-  let hourRotation = 30 * hh + mm / 2;
-  let minutesRotation = 6 * mm;
-  let secondsRotation = 6 * ss;
-
-  hr.style.transform = `rotate(${hourRotation}deg)`;
-  mn.style.transform = `rotate(${minutesRotation}deg)`;
-  sc.style.transform = `rotate(${secondsRotation}deg)`;
-}
-setInterval(localTime, 1000);
-
 function ChangeHomepageTime() {
   let BrusselsElement = document.querySelector("#Brussels");
   let CairoElement = document.querySelector("#Cairo");
@@ -78,3 +41,72 @@ function ChangeHomepageTime() {
 }
 
 setInterval(ChangeHomepageTime, 1000);
+
+function localTime(event) {
+  let locationCity = document.querySelector("#loc_city");
+  let locationTZ = moment.tz.guess();
+  let cityTime = moment().tz(locationTZ);
+  locationCity = locationTZ.replace("_", " ").split("/")[1];
+
+  let locationElement = document.getElementById("location");
+
+  if (locationElement) {
+    locationElement.innerHTML = `<div class="location" id="location">
+        <div class="time" id="loc_time">${cityTime.format(
+          "h:mm:ss"
+        )}<small> ${cityTime.format("A")}</small></div>
+        <div class="city" id="loc_city">${locationCity}</div>
+        <div class="date" id="loc_date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>`;
+    Clock(cityTime);
+  }
+
+  if (event) {
+    UpdateCity(event);
+  }
+}
+
+setInterval(localTime, 1000);
+function Clock(time) {
+  let hr = document.getElementById("hour");
+  let mn = document.getElementById("min");
+  let sc = document.getElementById("sec");
+
+  let hh = time.hour();
+  let mm = time.minute();
+  let ss = time.second();
+
+  let hourRotation = 30 * hh + mm / 2;
+  let minutesRotation = 6 * mm;
+  let secondsRotation = 6 * ss;
+
+  hr.style.transform = `rotate(${hourRotation}deg)`;
+  mn.style.transform = `rotate(${minutesRotation}deg)`;
+  sc.style.transform = `rotate(${secondsRotation}deg)`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", localTime);
+citiesSelectElement.addEventListener("change", UpdateCity);
+
+function UpdateCity(event) {
+  let cityTZ = event.target.value;
+  let updatedCityTime = moment().tz(cityTZ);
+  let cityName = cityTZ.replace("_", " ").split("/")[1];
+
+  let updatedCity = document.querySelector("#location");
+
+  if (cityTZ) {
+    updatedCity.innerHTML = `<div class="time" id="loc_time">${updatedCityTime.format(
+      "h:mm:ss"
+    )}<small>${updatedCityTime.format("A")}</small>
+  </div>
+        <div class="city" id="loc_city">${cityName}</div>
+        <div class="date" id="loc_date">${updatedCityTime.format(
+          "MMMM Do YYYY"
+        )}</div>
+      </div>`;
+
+    Clock(updatedCityTime);
+  }
+}
