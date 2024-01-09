48a3833ed54e185b7c1140c4ceb716d9babdc3cd
IlsaKQureshi <ilsakq.codes@gmail.com>
2024-01-09 23:46:33 +0500

    Challenge 2 change color in buttons

--- SheCodes AI Add-on/Week 1/src/script.js
+++ SheCodes AI Add-on/Week 1/src/script.js
@@ -1,10 +1,35 @@
-function alertAns(response) {
-  alert(response.data.answer);
+//function alertAns(response) {
+//  alert(response.data.answer);
+//}
+
+//let api = "bc8d89bca04963f6034ob7f2f43ta0c9";
+//let prompt = "Who the first female president was using the SheCodes AI API";
+//let context = "provide a clear and precise answer";
+//let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api}`;
+
+//axios.get(url).then(alertAns);
+
+//Second Challenge function handleClick(response) {}
+
+let disabledButton = document.querySelector("#disable-button");
+disabledButton.addEventListener("click", handleClick);
+
+function handleClick() {
+  disabledButton.setAttribute("disabled", "disabled");
 }
 
-let api = "bc8d89bca04963f6034ob7f2f43ta0c9";
-let prompt = "Who the first female president was using the SheCodes AI API";
-let context = "provide a clear and precise answer";
-let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api}`;
+let greenButton = document.querySelector("#green-button");
 
-axios.get(url).then(alertAns);
+greenButton.addEventListener("click", greenBgClick);
+
+function greenBgClick() {
+  greenButton.style.backgroundColor = "green";
+}
+
+let changeButton = document.querySelector("#change-colour");
+
+changeButton.addEventListener("click", colourChange);
+
+function colourChange() {
+  changeButton.classList.add("danger");
+}
