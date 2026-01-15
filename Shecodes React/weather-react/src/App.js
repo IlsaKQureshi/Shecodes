e4552e787d57595fb3882a0d0ae00a2238b76dbf
Ilsa Khurshid Qureshi <124763286+IlsaKQureshi@users.noreply.github.com>
2026-01-15 14:08:52 +0100

    converted sandbox to vs code to deploy on netlify

--- Shecodes React/weather-react/src/App.js
+++ Shecodes React/weather-react/src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-        <p>
-          Hello World
-        </p>
+        <Weather />
 
       </header>
     </div>
